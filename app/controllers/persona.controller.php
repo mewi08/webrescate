c7a26cfcbf4a44a41f36cde92619c386fbea15a1
Melanie Elizabeth <melanietello2020@gmail.com>
2025-12-30 22:45:20 -0500

    Métodos y controllers personas terminado

---
+++ app/controllers/persona.controller.php
@@ -0,0 +1,49 @@
+<?php
+require_once '../models/Persona.php';
+$persona = new Persona();
+
+  switch($_POST['operacion']){
+    case 'listar':
+      $registros = $persona->listar();
+      echo json_encode($registros);
+      break;
+
+    case 'registrar':
+      $registro = [
+        'dni'        => $_POST['dni'],
+        'nombres'    => $_POST['nombres'],
+        'apellidos'  => $_POST['apellidos'],
+        'telefono'   => $_POST['telefono'],
+        'email'      => $_POST['email']
+      ];
+      $idobtenido = $persona->agregar($registro);
+      echo json_encode(['idpersona'=> $idobtenido]);
+      break;  
+
+    case 'actualizar':
+     $registro = [
+        'dni'        => $_POST['dni'],
+        'nombres'    => $_POST['nombres'],
+        'apellidos'  => $_POST['apellidos'],
+        'telefono'   => $_POST['telefono'],
+        'email'      => $_POST['email'],
+        'idpersona'  => $_POST['idpesona']
+      ];
+      $filasafectadas = $persona->actualizar($registro);
+      echo json_encode(['filas'=> $filasafectadas]);
+      break;
+
+    case 'eliminar':
+      $filasafectadas= $persona->eliminar($_POST['idpersona']);
+      echo json_encode(['filas'=> $filasafectadas]);
+      break; 
+
+    case 'buscarPorID':
+        echo json_encode($persona->buscarPorId($_POST['idpersona']));
+        break;
+        
+    case 'buscarPorDni':
+        echo json_encode($persona->buscarPorDni($_POST['dni']));
+        break;
+  }
+
---
+++ app/models/Persona.php
@@ -0,0 +1,125 @@
+<?php
+
+require_once 'Conexion.php';
+
+class Persona extends Conexion{
+    private $conexion;
+    
+    public function __construct(){
+        $this->conexion = parent::getConexion();
+    }
+
+    public function listar(): array{
+        try{
+            $sql = "
+            SELECT idpersona, dni, nombres, apellidos,telefono,email 
+            FROM personas
+            ORDER BY idpersona DESC";
+
+            $consulta = $this->conexion->prepare($sql);
+            
+            $consulta->execute();
+
+            return $consulta->fetchAll(PDO::FETCH_ASSOC);
+        }catch(Exception $e){
+            return [];
+        }
+    }
+
+    public function agregar($registro=[]): int{
+        try{
+            $sql = "
+            INSERT INTO personas (dni, nombres, apellidos,telefono,email) 
+            VALUES (?,?,?,?,?)";
+
+            $consulta = $this->conexion->prepare($sql);
+            $consulta->execute(
+                array(
+                    $registro ['dni'],
+                    $registro ['nombres'],
+                    $registro ['apellidos'],
+                    $registro ['telefono'],
+                    $registro ['email']
+                )
+            );
+            return $consulta->rowCount();
+        }catch(Exception $e){
+            return -1;
+        }
+    }
+    
+    public function eliminar($id):int{
+        try{
+            $sql = "UPDATE personas SET activo=0 WHERE idpersona = ?";
+
+            $consulta = $this->conexion->prepare($sql);
+            
+            $consulta->execute(
+                array($id)
+            );
+            return $consulta->rowCount();
+        }catch(Exception $e){
+            return -1;
+        }
+    }
+
+    public function actualizar($registro = []){
+        try{
+            $sql="
+            UPDATE personas SET
+                dni = ?, 
+                nombres = ?,
+                apellidos = ?,
+                telefono = ?,
+                email = ?,
+                updated = now()
+            WHERE idpersona=? 
+            ";
+            $consulta = $this->conexion->prepare($sql);
+
+            $consulta->execute(
+                array(
+                    $registro['dni'],
+                    $registro['nombres'],
+                    $registro['apellidos'],
+                    $registro['telefono'],
+                    $registro['email'],
+                    $registro['idpersona']
+                )
+            );
+            return $consulta->rowCount();
+        }catch(Exception $e){
+            return -1;
+        }
+    }
+    public function buscarPorId($id){
+         try{
+            $sql = "SELECT * FROM personas WHERE idpersona = ? AND activo=1";
+
+            $consulta = $this->conexion->prepare($sql);
+            
+            $consulta->execute(
+                array($id)                
+            );
+
+            return $consulta->fetchAll(PDO::FETCH_ASSOC);
+        }catch(Exception $e){
+           die($e->getMessage());
+        }
+    }
+    public function buscarPorDni($dni){
+        try{
+           $sql = "SELECT * FROM personas WHERE dni LIKE ? AND activo=1";
+
+            $consulta = $this->conexion->prepare($sql);
+            
+            $consulta->execute(
+                array("%$dni%")                
+            );
+
+            return $consulta->fetchAll(PDO::FETCH_ASSOC);
+        }catch(Exception $e){
+            die($e->getMessage());
+        }
+    }
+}
